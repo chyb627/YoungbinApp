4dec8ad052d9cb6e4911b6f4203128eee8e142ba
cha <chyb627@google.com>
2022-07-12 17:51:22 +0900

    tailwindcss + prettier 설정

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-/**
- * @format
- */
-
+import 'babel-plugin-tailwind-rn/dist/useTailwind';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
